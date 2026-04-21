28fcdb3376fe08a71b8ab1bcabca0c06d7d5f11e
deirdre503 <wang.haoran10@northeastern.edu>
2026-04-21 01:49:08 -0700

    changed base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CS5610-26Spring-HaoranWang-QilinZeng-Project3/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
